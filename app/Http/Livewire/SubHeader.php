f93c2d1388dd80fbf9dc7c90e2ca69f9cf7364dc
Manwar Hossain <itsmanwar@gmail.com>
2022-12-03 18:22:17 +0530

    Update SubHeader.php

--- app/Http/Livewire/SubHeader.php
+++ app/Http/Livewire/SubHeader.php
@@ -7,11 +7,20 @@ use Livewire\Component;
 class SubHeader extends Component
 {
     public $createButtonOn = false;
-    protected $listeners = ['openForm' => 'CCButtonControl'];
+    public $titel = 'UPMS',$subTitel;
+    protected $listeners = ['openForm' => 'CCButtonControl','changeTitel'=>'setTitel','changeSubTitel'=>'setSubTitel'];
 
     public function CCButtonControl()
     {
         $this->createButtonOn = !$this->createButtonOn;
+    }
+    public function setTitel($titel)
+    {
+        $this->titel = $titel;
+    }
+    public function setSubTitel($subTitel)
+    {
+        $this->subTitel = $subTitel;
     }
     public function render()
     {
