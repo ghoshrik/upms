b8d85dee270c1edd6ec19a3af843e700ed8f37d5
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-03-17 16:50:44 +0530

    office model update level

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -10,7 +10,7 @@ class Office extends Model
     use HasFactory;
     protected $table = "offices";
     protected $fillable = [
-        'office_name','department_id','dist_code','In_area','rural_block_code','gp_code','urban_code','ward_code','office_address'
+        'office_name','department_id','dist_code','In_area','rural_block_code','gp_code','urban_code','ward_code','office_address','level'
     ];
     public function getDepartmentName()
     {
