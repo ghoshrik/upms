652a93256421eac3d4e8d49d285fda2e854075fd
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-03-21 17:24:20 +0530

    validation user management

--- app/Http/Livewire/UserManagement/CreateUser.php
+++ app/Http/Livewire/UserManagement/CreateUser.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Livewire\UserManagement;
 
-use App\Models\Department;
-use App\Models\Designation;
-use App\Models\Office;
 use App\Models\User;
-use App\Models\UserType;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use App\Models\Office;
 use Livewire\Component;
+use App\Models\UserType;
+use App\Models\Department;
 use WireUi\Traits\Actions;
+use App\Models\Designation;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Support\Facades\Hash;
 use function PHPUnit\Framework\isEmpty;
 
 class CreateUser extends Component
@@ -19,14 +20,46 @@ class CreateUser extends Component
     use Actions;
     public $dropDownData = [], $newUserData = [], $selectLevel;
 
-    // protected $rules = [
-    //     'newUserData.email'=>'required|unique:users,email',
-    //     'newUserData.phone'=>'required',
-    // ];
-    // protected $messages = [
-    //     'newUserData.email.required'=>'Email Field is required',
-    //     'newUserData.phone.required'=>'phone number field is required',
-    // ];
+    protected $rules = [
+        'newUserData.email' => 'required|unique:users,email',
+        // 'newUserData.emp_id'=>'required|numeric'
+        'newUserData.username' => 'required|string',
+        'newUserData.emp_name' => 'required|string',
+        'newUserData.department_id' => 'required|integer',
+        'newUserData.mobile' => 'required|numeric|min:10',
+
+    ];
+    protected $messages = [
+        'newUserData.email.required' => 'Email Field is required',
+        'newUserData.email.unique' => 'The email address is already in use.',
+        'newUserData.username.required' => 'username is required',
+        'newUserData.username.string' => 'username must be string',
+        'newUserData.emp_name.required' => 'employee name is required',
+        'newUserData.emp_name.string' => 'error',
+        'newUserData.department_id.required' => 'please select department',
+        'newUserData.department_id.integer' => 'do not format',
+        'newUserData.mobile.required' => 'The mobile number is required',
+        'newUserData.mobile.numeric' => 'The mobile number must be a number',
+        'newUserData.mobile.min' => 'The mobile number must be exactly 10 digits long',
+        'newUserData.office_id.required' => 'The office name must be required',
+        'newUserData.office_id.integer' => 'data mismatch',
+        'selectLevel.required' => 'The Office level must be required',
+
+        // 'selectLevel.'
+    ];
+
+
+    public function booted()
+    {
+        // if($newUserData.office_id)
+        if (Auth::user()->user_type == 3) {
+            $this->rules =  Arr::collapse([$this->rules, [
+                'newUserData.office_id' => 'required|integer',
+                'selectLevel' => 'required|unique'
+            ]]);
+        }
+        // if (Auth::user()->user_type == 3) {
+    }
 
 
     public function mount()
@@ -40,8 +73,8 @@ class CreateUser extends Component
             'username' => '',
             'password' => '',
             'confirm_password' => '',
-            'mobile'=>'',
-            'email'=>'',
+            'mobile' => '',
+            'email' => '',
         ];
         if (Auth::user()->user_type == 2) {
             $this->getDropdownData('DEPT');
@@ -64,10 +97,9 @@ class CreateUser extends Component
                 } else {
                     $this->dropDownData['departments'] =  Department::all();
                 }
-            }elseif($lookingFor === 'LEVEL'){
+            } elseif ($lookingFor === 'LEVEL') {
                 $this->dropDownData['level'] = true;
-            }
-            else {
+            } else {
                 // $this->allUserTypes = UserType::where('parent_id', Auth::user()->user_type)->get();
             }
         } catch (\Throwable $th) {
@@ -76,47 +108,44 @@ class CreateUser extends Component
     }
     public function fetchLevelWiseOffice()
     {
-        if($this->selectLevel)
-        {
-            $this->dropDownData['offices'] = Office::where([['department_id', Auth::user()->department_id],['level',$this->selectLevel]])->get();
+        if ($this->selectLevel) {
+            $this->dropDownData['offices'] = Office::where([['department_id', Auth::user()->department_id], ['level', $this->selectLevel]])->get();
         }
     }
     public function store()
     {
-        // $this->validate();
-        try{
-        // TODO::INSERT THE DEPT. ID NAD OFFICE ID .
-                unset($this->newUserData['confirm_password']);
-                $userType =  UserType::where('parent_id', Auth::user()->user_type)->first();
-                if (isset($userType)) {
-                    $this->newUserData['user_type'] = $userType['id'];
-                    $this->newUserData['department_id'] = (Auth::user()->user_type==2)? $this->newUserData['department_id'] : Auth::user()->department_id;
-                    $this->newUserData['designation_id'] = ($this->newUserData['designation_id'] == '') ? Auth::user()->designation_id : $this->newUserData['designation_id'];
-                    $this->newUserData['office_id'] = ($this->newUserData['office_id'] == '')? Auth::user()->office_id : $this->newUserData['office_id'];
-                    $this->newUserData['email'] = $this->newUserData['email'];
-                    $this->newUserData['mobile'] = $this->newUserData['mobile'];
-                    // $this->newUserData['password'] = Hash::make($this->newUserData['password']);
-                    $this->newUserData['password'] = Hash::make('password');
-                    // dd($this->newUserData);
-                    if (User::create($this->newUserData)) {
-                        $this->notification()->success(
-                            $title = 'Success',
-                            $description =  trans('cruds.user-management.create_msg')
-                        );
-                        $this->reset();
-                        $this->emit('openEntryForm');
-                        return;
-                    }
+        $this->validate();
+        try {
+            // TODO::INSERT THE DEPT. ID NAD OFFICE ID .
+            unset($this->newUserData['confirm_password']);
+            $userType =  UserType::where('parent_id', Auth::user()->user_type)->first();
+            if (isset($userType)) {
+                $this->newUserData['user_type'] = $userType['id'];
+                $this->newUserData['department_id'] = (Auth::user()->user_type == 2) ? $this->newUserData['department_id'] : Auth::user()->department_id;
+                $this->newUserData['designation_id'] = ($this->newUserData['designation_id'] == '') ? Auth::user()->designation_id : $this->newUserData['designation_id'];
+                $this->newUserData['office_id'] = ($this->newUserData['office_id'] == '') ? Auth::user()->office_id : $this->newUserData['office_id'];
+                $this->newUserData['email'] = $this->newUserData['email'];
+                $this->newUserData['mobile'] = $this->newUserData['mobile'];
+                // $this->newUserData['password'] = Hash::make($this->newUserData['password']);
+                $this->newUserData['password'] = Hash::make('password');
+                // dd($this->newUserData);
+                if (User::create($this->newUserData)) {
+                    $this->notification()->success(
+                        $title = 'Success',
+                        $description =  trans('cruds.user-management.create_msg')
+                    );
+                    $this->reset();
+                    $this->emit('openEntryForm');
+                    return;
                 }
-                $this->notification()->error(
-                    $title = 'Error !!!',
-                    $description = 'Something went wrong.'
-                );
             }
-
-            catch (\Throwable $th) {
-                $this->emit('showError', $th->getMessage());
-            }
+            $this->notification()->error(
+                $title = 'Error !!!',
+                $description = 'Something went wrong.'
+            );
+        } catch (\Throwable $th) {
+            $this->emit('showError', $th->getMessage());
+        }
     }
     public function render()
     {
