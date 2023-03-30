8144a42c2b847827eea0862b1184964fda6c2fdd
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-03-30 17:10:35 +0530

    icon change sor download

--- resources/views/components/data-table-components/buttons/sorDownload.blade.php
+++ resources/views/components/data-table-components/buttons/sorDownload.blade.php
@@ -1,3 +1,3 @@
 <button wire:click="generatePdf({{ $value }})" type="button" class="btn btn-soft-secondary btn-sm">
-    <x-icon name="document" class="w-5 h-5" />Download
+    <x-icon name="download" class="w-5 h-5" />Download
 </button>
