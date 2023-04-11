60a92eadf63fe750601d45c43860f02965d2dbf0
Test <test@gmail.com>
2023-04-11 16:46:30 +0530

    Update assign-to-another-office.blade.php

--- resources/views/livewire/assign-to-another-office/assign-to-another-office.blade.php
+++ resources/views/livewire/assign-to-another-office/assign-to-another-office.blade.php
@@ -68,40 +68,9 @@
                             <div class="row row-cols-1">
                                 <div class="d-slider1 overflow-hidden ">
                                     <div class="row">
-                                        <div class="col-md-4">
-                                            <li class="swiper-slide card card-tab card-slide {{ $this->selectedTab == 1 ? 'active' : '' }}"
-                                                wire:click="draftData()">
-                                                <div class="card-body">
-                                                    <div class="progress-widget">
-                                                        <div id="circle-progress-01"
-                                                            class="circle-progress-01 circle-progress circle-progress-primary text-center"
-                                                            data-min-value="0"
-                                                            data-max-value="{{ $counterData['totalDataCount'] }}"
-                                                            data-value="{{ $counterData['draftDataCount'] }}"
-                                                            data-type="percent" wire:ignore>
-                                                            <svg class="card-slie-arrow" width="24" height="24px"
-                                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                                stroke-width="2" stroke-linecap="round"
-                                                                stroke-linejoin="round">
-                                                                <rect x="2" y="4" width="20"
-                                                                    height="5" rx="2"></rect>
-                                                                <path d="M4 9v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9">
-                                                                </path>
-                                                                <path d="M10 13h4"></path>
-                                                            </svg>
-                                                        </div>
-                                                        <div class="progress-detail" wire:key="$updateDataTableTracker">
-                                                            <p class="mb-2">Total Draft</p>
-                                                            <h4 class="counter" style="visibility: visible;">
-                                                                {{ $counterData['draftDataCount'] }}</h4>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                        </div>
-                                        <div class="col-md-4">
+                                        <div class="col-md-6">
                                             <li class="swiper-slide card card-tab card-slide {{ $this->selectedTab == 2 ? 'active' : '' }}"
-                                                wire:click='forwardedData()'>
+                                                wire:click='requestData()'>
                                                 <div class="card-body">
                                                     <div class="progress-widget">
                                                         <div id="circle-progress-02"
@@ -119,7 +88,7 @@
                                                             </svg>
                                                         </div>
                                                         <div class="progress-detail">
-                                                            <p class="mb-2"> Total Forwarded</p>
+                                                            <p class="mb-2"> Total Request</p>
                                                             <h4 class="counter" style="visibility: visible;"
                                                                 wire:key="$updateDataTableTracker">
                                                                 {{ $counterData['forwardedDataCount'] }}</h4>
@@ -128,7 +97,7 @@
                                                 </div>
                                             </li>
                                         </div>
-                                        <div class="col-md-4">
+                                        <div class="col-md-6">
                                             <li class="swiper-slide card card-tab card-slide {{ $this->selectedTab == 3 ? 'active' : '' }}"
                                                 wire:click="revertedData()">
                                                 <div class="card-body">
@@ -171,8 +140,7 @@
                             @elseif ($this->selectedTab == 2)
                                 <div class="card">
                                     <div class="card-body">
-                                        {{-- <livewire:estimate-project.data-table.forwarded-estimate-project-table :wire:key="$updateDataTableTracker" /> --}}
-                                        {{-- <livewire:estimate-project.datatable.powergrid.forwarded-estimate-project-table :wire:key="$updateDataTableTracker" /> --}}
+                                        <livewire:assign-to-another-office.data-table.other-office-assign-request-table/>
                                     </div>
                                 </div>
                             @elseif ($this->selectedTab == 3)
