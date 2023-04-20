70777cd7ce5d8982139c67c37ad17b066d06d59c
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-04-20 14:15:29 +0530

    cruds trads updated

--- resources/views/livewire/action-components/sor/sor-prepare-table-buttons.blade.php
+++ resources/views/livewire/action-components/sor/sor-prepare-table-buttons.blade.php
@@ -1,3 +1,6 @@
-@include('components.data-table-components.buttons.sorDownload')
-@include('components.data-table-components.buttons.edit')
-
+<div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="btn-group mr-2 btn-group-sm" role="group" aria-label="First group">
+        @include('components.data-table-components.buttons.sorDownload')
+        @include('components.data-table-components.buttons.edit')
+    </div>
+</div>
