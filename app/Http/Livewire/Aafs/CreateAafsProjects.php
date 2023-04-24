8ff5ff69b527975b19d055e74c4242755e62f487
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-04-24 10:13:07 +0530

    aafs table updated

--- app/Http/Livewire/Aafs/CreateAafsProjects.php
+++ app/Http/Livewire/Aafs/CreateAafsProjects.php
@@ -3,8 +3,10 @@
 namespace App\Http\Livewire\Aafs;
 
 use App\Models\AAFS;
+use App\Models\Department;
 use Livewire\Component;
 use App\Models\SorMaster;
+use App\Models\Tender;
 use Illuminate\Support\Facades\Storage;
 use WireUi\Traits\Actions;
 use Livewire\WithFileUploads;
@@ -14,15 +16,32 @@ class CreateAafsProjects extends Component
     use WithFileUploads;
     use Actions;
     // public $photo,$goId, $projectId, $goDate;
-    public $InputStore = [], $projects_number = [];
+    public $InputStore = [], $projects_number = [], $departmentList = [];
 
 
 
     protected $rules = [
-        'InputStore.photo' => 'required',
-        'InputStore.goId' => 'required|numeric',
+        // 'InputStore.photo' => 'required',
+        // 'InputStore.goId' => 'required|numeric',
         'InputStore.projectId' => 'required|integer',
-        'InputStore.goDate' => 'required',
+        'InputStore.departmentId' => 'required|integer',
+        'InputStore.progressStatus' => 'required',
+        'InputStore.projectAmount' => 'required|numeric',
+        'InputStore.tenderAmount' => 'required|numeric',
+        // 'InputStore.goDate' => 'required',
+
+        'InputStore.aafsMotherId' => 'required|numeric',
+        'InputStore.aafsSubId' => 'required|numeric',
+        'InputStore.projectType' => 'required',
+        'InputStore.status' => 'required',
+        'InputStore.completePeriod' => 'required',
+        'InputStore.unNo' => 'required',
+        'InputStore.goNo' => 'required',
+        'InputStore.preaafsExp' => 'required',
+        'InputStore.postaafsExp' => 'required',
+        'InputStore.Fundcty' => 'required',
+        'InputStore.exeAuthority' => 'required',
+
     ];
     protected $messages = [
         'InputStore.photo.required' => 'This field is required',
@@ -31,6 +50,30 @@ class CreateAafsProjects extends Component
         'InputStore.projectId.required' => 'This project Number field is required',
         'InputStore.projectId.integer' => 'Data format mismatch',
         'InputStore.goDate.required' => 'This GO date field is required',
+        'InputStore.departmentId.required' => 'This field is required',
+        'InputStore.departmentId.integer' => 'This format Not Valid',
+        'InputStore.progressStatus.required' => 'This field is required',
+        // 'InputStore.progressStatus.integer' => 'Data Format Not Allow',
+        'InputStore.projectAmount.required' => 'This field is required',
+        'InputStore.projectAmount.numeric' => 'This field only allow number',
+        'InputStore.tenderAmount.numeric' => 'This field only allow number',
+        'InputStore.tenderAmount.required' => 'This field is required',
+
+
+        'InputStore.aafsMotherId.required' => 'This field is required',
+        'InputStore.aafsSubId.required' => 'This field is required',
+        'InputStore.aafsMotherId.numeric' => 'Only Allow number',
+        'InputStore.aafsSubId.numeric' => 'Only Allow number',
+        'InputStore.projectType.required' => 'This field is required',
+        'InputStore.status.required' => 'This field is required',
+        'InputStore.completePeriod.required' => 'This field is required',
+        'InputStore.unNo.required' => 'This field is required',
+        'InputStore.goNo.required' => 'This field is required',
+        'InputStore.preaafsExp.required' => 'This field is required',
+        'InputStore.postaafsExp.required' => 'This field is required',
+        'InputStore.Fundcty.required' => 'This field is required',
+        'InputStore.exeAuthority.required' => 'This field is required',
+
         // 'InputStore.goDate.date_format' => 'This field must be valid only date format'
     ];
 
@@ -40,26 +83,70 @@ class CreateAafsProjects extends Component
     }
     public function mount()
     {
-        $this->projects_number = SorMaster::where('is_verified', '=', 1)->get();
+        $this->departmentList = Department::OrderBy('id', 'desc')->get();
+        // $this->projects_number = Tender::select('project_no')->get();
+        // select('tenders.project_no as projects_number')
+        // ->join('sor_masters', 'sor_masters.estimate_id', '=', 'tenders.project_no')
+        // ->join('estimate_prepares', 'estimate_prepares.estimate_id', '=', 'sor_masters.estimate_id')
+        // ->join('estimate_statuses', 'estimate_statuses.id', '=', 'sor_masters.status')
+        // ->where('sor_masters.is_verified', '=', 1)
+        // ->where('estimate_prepares.operation', '=', 'Total')
+        // ->get();
+
         $this->InputStore = [
-            'photo' => '',
-            'goId' => '',
+            // 'photo' => '',
+            // 'goId' => '',
             'projectId' => '',
-            'goDate' => ''
+            // 'goDate' => '',
+            'departmentId' => '',
+            'progressStatus' => '',
+            'projectAmount' => '',
+            'tenderAmount' => '',
+            'aafsMotherId' => '',
+            'aafsSubId' => '',
+            'projectType' => '',
+            'status' => '',
+            'completePeriod' => '',
+            'unNo' => '',
+            'goNo' => '',
+            'preaafsExp' => '',
+            'postaafsExp' => '',
+            'Fundcty' => '',
+            'exeAuthority' => ''
+
+
         ];
     }
     public function store()
     {
+        // dd($this->projectDtls['status_id']);
 
         $this->validate();
         try {
             $insert = [
-                'project_id' => $this->InputStore['projectId'],
-                'Go_id' => $this->InputStore['goId'],
-                'go_date' => $this->InputStore['goDate'],
-                'support_data' => base64_encode(file_get_contents($this->InputStore['photo']->getRealPath())),
-                'status' => 0,
+                'project_no' => $this->InputStore['projectId'],
+                // 'Go_id' => $this->InputStore['goId'],
+                // 'go_date' => $this->InputStore['goDate'],
+                // 'support_data' => base64_encode(file_get_contents($this->InputStore['photo']->getRealPath())),
+                // 'status' => 0,
+                'dept_id' => $this->InputStore['departmentId'],
+                'status_id' => $this->InputStore['currentProStatus'],
+                'project_cost' => $this->InputStore['projectAmount'],
+                'tender_cost' => $this->InputStore['tenderAmount'],
+
+                'aafs_mother_id' => $this->InputStore['aafsMotherId'],
+                'aafs_sub_id' => $this->InputStore['aafsSubId'],
+                'project_type' => $this->InputStore['projectType'],
+                'status' => $this->InputStore['status'],
+                'completePeriod' => $this->InputStore['completePeriod'],
+                'unNo' => $this->InputStore['unNo'],
+                'goNo' => $this->InputStore['goNo'],
+                'preaafsExp' => $this->InputStore['preaafsExp'],
+                'postaafsExp' => $this->InputStore['postaafsExp'],
+                'Fundcty' => $this->InputStore['Fundcty'],
+                'exeAuthority' => $this->InputStore['exeAuthority']
             ];
+            // dd($insert);
             AAFS::create($insert);
 
             $this->notification()->success(
@@ -71,8 +158,30 @@ class CreateAafsProjects extends Component
             $this->emit('showError', $th->getMessage());
         }
     }
+    public $projectDtls;
+    public function getProjectDetails()
+    {
+        // dd($this->InputStore['projectId']);
+
+        $this->projectDtls = Tender::select('tenders.project_no as projects_number', 'estimate_prepares.total_amount as project_total_amount', 'estimate_statuses.status as project_status', 'tenders.tender_amount as tender_total_amount', 'estimate_statuses.id as status_id')
+            ->join('sor_masters', 'sor_masters.estimate_id', '=', 'tenders.project_no')
+            ->join('estimate_prepares', 'estimate_prepares.estimate_id', '=', 'sor_masters.estimate_id')
+            ->join('estimate_statuses', 'estimate_statuses.id', '=', 'sor_masters.status')
+            ->where('sor_masters.is_verified', '=', 1)
+            ->where('estimate_prepares.operation', '=', 'Total')
+            ->where('tenders.project_no', $this->InputStore['projectId'])
+            ->first();
+        $this->InputStore['projectId'] = $this->projectDtls['projects_number'];
+        $this->InputStore['progressStatus'] = $this->projectDtls['project_status'];
+        $this->InputStore['projectAmount'] = $this->projectDtls['project_total_amount'];
+        $this->InputStore['tenderAmount'] = $this->projectDtls['tender_total_amount'];
+        $this->InputStore['currentProStatus'] = $this->projectDtls['status_id'];
+
+        // dd($this->InputStore['projectId']);
+    }
     public function render()
     {
+        $this->projects_number = Tender::select('project_no')->get();
         return view('livewire.aafs.create-aafs-projects');
     }
 }
--- resources/views/livewire/aafs/create-aafs-projects.blade.php
+++ resources/views/livewire/aafs/create-aafs-projects.blade.php
@@ -2,19 +2,91 @@
     <x-form-section submit='store'>
         <x-slot name='form'>
             <div class="row">
-                <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
-                    <x-select label="{{ trans('cruds.aafs_project.fields.proj_id') }}"
-                        placeholder="Select one {{ trans('cruds.aafs_project.fields.proj_id') }}"
-                        wire:model.defer="InputStore.projectId">
-
-                        @foreach ($projects_number as $projects)
-                            InputStore
-                            <x-select.option label="{{ $projects['estimate_id'] }}"
-                                value="{{ $projects['estimate_id'] }}" />
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-select label="{{ trans('cruds.aafs_project.fields.department_id') }}"
+                        placeholder="Select one {{ trans('cruds.aafs_project.fields.department_id') }}"
+                        wire:model.defer="InputStore.departmentId">
+                        @foreach ($departmentList as $department)
+                            <x-select.option label="{{ $department['department_name'] }} "
+                                value="{{ $department['id'] }}" />
                         @endforeach
                     </x-select>
                 </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-select label="{{ trans('cruds.aafs_project.fields.proj_id') }}"
+                        placeholder="Select one {{ trans('cruds.aafs_project.fields.proj_id') }}"
+                        wire:model.defer="InputStore.projectId" x-on:select="$wire.getProjectDetails()">
+                        @foreach ($projects_number as $projects)
+                            <x-select.option label="{{ $projects['project_no'] }} "
+                                value="{{ $projects['project_no'] }}" />
+                        @endforeach
+                    </x-select>
+                    {{-- @dd($projects_number) --}}
+                </div>
+                {{-- @isset($projectDtls) --}}
+                <div class="col-md-2 col-lg-2 col-sm-3 mb-2">
+                    <x-input label="Status of Progress" wire:model.defer="InputStore.progressStatus"
+                        placeholder="Status of Progress" readonly />
+                </div>
+                <div class="col-md-2 col-lg-2 col-sm-3 mb-2">
+                    <x-input right-icon="currency-rupee" label="Project Cost"
+                        wire:model.defer="InputStore.projectAmount" placeholder="Project Cost" readonly />
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-sm-3 mb-2">
+                    <x-input right-icon="currency-rupee" label="Tender Cost" wire:model.defer="InputStore.tenderAmount"
+                        placeholder="Troject Cost" readonly />
+                </div>
+                {{-- @endisset --}}
+
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="AAFS Project ID(Mother project ID)" wire:model='InputStore.aafsMotherId'
+                        placeholder="Enter AAFS Project ID(Mother project ID)" />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="AAFS Project ID(sub project ID)" wire:model='InputStore.aafsSubId'
+                        placeholder="Enter AAFS Project ID(sub project ID)" />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="Project Type" wire:model='InputStore.projectType' placeholder="Project Type" />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="Status as per AAFS" wire:model='InputStore.status'
+                        placeholder="Status as per AAFS " />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="Completion Period" wire:model='InputStore.completePeriod'
+                        placeholder="Completion Period " />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="UO no and Date" wire:model='InputStore.unNo' placeholder="Enter UO no and Date" />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="GO no and Date" wire:model='InputStore.goNo' placeholder="GO no and Date" />
+                </div>
+                <div class="col-md-3 col-lg-3 col-sm-3 mb-2">
+                    <x-input label="Pre AAFS Expenditure" wire:model='InputStore.preaafsExp'
+                        placeholder="Pre AAFS Expenditure" />
+                </div>
                 <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
+                    <x-input label="Post Expenditure Incurred" wire:model='InputStore.postaafsExp'
+                        placeholder="Post Expenditure Incurred" />
+                </div>
+                <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
+                    <x-input label="Fund released in CFY but Expenditure not yet incurred"
+                        wire:model='InputStore.Fundcty'
+                        placeholder="Fund released in CFY but Expenditure not yet incurred" />
+                </div>
+                <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
+                    <x-input label="Executing Authority" wire:model='InputStore.exeAuthority'
+                        placeholder="Executing Authority" />
+                </div>
+
+
+
+
+
+                {{-- <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
                     <x-input label="{{ trans('cruds.aafs_project.fields.Govt_id') }}" wire:model='InputStore.goId'
                         placeholder="Enter {{ trans('cruds.aafs_project.fields.Govt_id') }}" />
                 </div>
@@ -22,27 +94,26 @@
                     <x-datetime-picker without-time label="{{ trans('cruds.aafs_project.fields.go_date') }}"
                         placeholder="{{ trans('cruds.aafs_project.fields.go_date') }}"
                         wire:model.defer="InputStore.goDate" />
-                </div>
-                <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
-                    <div>
-                        {{-- <div class="flex justify-between mb-1">
+                </div> --}}
+                {{-- <div class="col-md-4 col-lg-4 col-sm-3 mb-2">
+                    <div> --}}
+                {{-- <div class="flex justify-between mb-1">
                             <label class="block text-sm font-medium text-secondary-700 dark:text-gray-400">
                                 File Upload
                             </label>
                         </div> --}}
-                        <div class="relative rounded-md shadow-sm">
-                            {{-- <input type="file" autocomplete="off" wire:model='InputStore.photo'
+                {{-- <div class="relative rounded-md shadow-sm"> --}}
+                {{-- <input type="file" autocomplete="off" wire:model='InputStore.photo'
                                 class="placeholder-secondary-400 dark:bg-secondary-800 dark:text-secondary-400 dark:placeholder-secondary-500 border border-secondary-300 focus:ring-primary-500 focus:border-primary-500 dark:border-secondary-600 form-input block w-full sm:text-sm rounded-md transition ease-in-out duration-100 focus:outline-none shadow-sm"> --}}
 
-                            <x-input type="file" wire:model="InputStore.photo" label="Choose file" autocomplete="off"
-                                class="placeholder-secondary-400 dark:bg-secondary-800 dark:text-secondary-400 dark:placeholder-secondary-500 border border-secondary-300 focus:ring-primary-500 focus:border-primary-500 dark:border-secondary-600 form-input block w-full sm:text-sm rounded-md transition ease-in-out duration-100 focus:outline-none shadow-sm" />
-                            {{-- @error('InputStore.photo')
+                {{-- <x-input type="file" wire:model="InputStore.photo" label="Choose file" autocomplete="off"
+                                class="placeholder-secondary-400 dark:bg-secondary-800 dark:text-secondary-400 dark:placeholder-secondary-500 border border-secondary-300 focus:ring-primary-500 focus:border-primary-500 dark:border-secondary-600 form-input block w-full sm:text-sm rounded-md transition ease-in-out duration-100 focus:outline-none shadow-sm" /> --}}
+                {{-- @error('InputStore.photo')
                                 <span class="error" style="color: red;">{{ $message }}</span>
                             @enderror --}}
-                        </div>
+                {{-- </div>
                         <div wire:loading wire:target="InputStore.photo">Uploading...</div>
-                    </div>
-                </div>
+                    </div> --}}
             </div>
             <div class="row">
                 <div class="col-12">
