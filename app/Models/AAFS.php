a48e8bc11ba8adc64e8567d0efe7c8f2111601d0
Test <test@gmail.com>
2023-04-25 17:25:19 +0530

    update aafs

--- app/Models/AAFS.php
+++ app/Models/AAFS.php
@@ -10,14 +10,32 @@ class AAFS extends Model
 {
     use HasFactory;
     protected $table = "aafs";
-    protected $fillable = ["project_id","Go_id","support_data","status","go_date"];
+    // protected $fillable = ["project_id","Go_id","support_data","status","go_date"];
 
-    protected $dates = ['go_date'];
+    // protected $dates = ['go_date'];
 
-    public function getMyDateAttribute($value)
-    {
-        $carbonDate = Carbon::createFromFormat('d/m/Y', $value); // Create a Carbon instance from the input date
-        $this->attributes['go_date'] = $carbonDate->format('Y-m-d'); // Convert the date to the desired format and set the attribute// Convert the date to the desired format
+    // public function getMyDateAttribute($value)
+    // {
+    //     $carbonDate = Carbon::createFromFormat('d/m/Y', $value); // Create a Carbon instance from the input date
+    //     $this->attributes['go_date'] = $carbonDate->format('Y-m-d'); // Convert the date to the desired format and set the attribute// Convert the date to the desired format
 
-    }
+    // }
+    protected $fillable = [
+        'project_no',
+        'dept_id',
+        'status_id',
+        'project_cost',
+        'tender_cost',
+        'aafs_mother_id',
+        'aafs_sub_id',
+        'project_type',
+        'status',
+        'completePeriod',
+        'unNo',
+        'goNo',
+        'preaafsExp',
+        'postaafsExp',
+        'Fundcty',
+        'exeAuthority'
+    ];
 }
