a6776cd5d02d18d3d389874f4c44eaca70c53008
bijoymandal <30389880+bijoymandal@users.noreply.github.com>
2023-04-26 17:57:19 +0530

    office powergrid table

--- app/Http/Livewire/Office/DataTable/OfficeTable.php
+++ app/Http/Livewire/Office/DataTable/OfficeTable.php
@@ -2,79 +2,300 @@
 
 namespace App\Http\Livewire\Office\DataTable;
 
-use Rappasoft\LaravelLivewireTables\DataTableComponent;
-use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Office;
-use Illuminate\Database\Eloquent\Builder;
+use WireUi\Traits\Actions;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+use PowerComponents\LivewirePowerGrid\Rules\{Rule, RuleActions};
+use PowerComponents\LivewirePowerGrid\{Button, Column, Exportable, Footer, Header, PowerGrid, PowerGridComponent, PowerGridEloquent};
 
-class OfficeTable extends DataTableComponent
+final class OfficeTable extends PowerGridComponent
 {
-    protected $model = Office::class;
+    use ActionButton, Actions;
 
-    public function configure(): void
+    /*
+    |--------------------------------------------------------------------------
+    |  Features Setup
+    |--------------------------------------------------------------------------
+    | Setup Table's general features
+    |
+    */
+    protected function getListeners()
     {
-        $this->setPrimaryKey('id');
+        return array_merge(
+            parent::getListeners(),
+            [
+                'rowActionEvent',
+                'bulkActionEvent',
+            ]
+        );
+    }
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
     }
 
+    public function header(): array
+    {
+        return [
+            Button::add('bulk-demo')
+                ->caption('PDF')
+                ->class('cursor-pointer btn btn-danger btn-sm')
+                ->emit('bulkActionEvent', [])
+        ];
+    }
+
+    public function bulkActionEvent(): void
+    {
+        if (count($this->checkboxValues) == 0) {
+            // $this->dispatchBrowserEvent('showAlert', ['message' => 'You must select at least one item!']);
+            $this->dialog()->error(
+                $title = 'Error !!!',
+                $description = 'You must select at least one item!'
+            );
+            return;
+        }
+
+        $ids = implode(', ', $this->checkboxValues);
+
+        $this->dispatchBrowserEvent('showAlert', ['message' => 'You have selected IDs: ' . $ids]);
+        $this->dialog()->error(
+            $title = 'Error !!!',
+            $description = 'You must select at least one item!'
+        );
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+
+    /**
+     * PowerGrid datasource.
+     *
+     * @return Builder<\App\Models\Office>
+     */
+    public function datasource(): Builder
+    {
+        return Office::query()
+            ->where('offices.department_id', Auth::user()->department_id);;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+
+    /**
+     * Relationship search.
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    | ❗ IMPORTANT: When using closures, you must escape any value coming from
+    |    the database using the `e()` Laravel Helper function.
+    |
+    */
+    public function addColumns(): PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            // ->addColumn('id')
+            ->addColumn('office_name')
+
+            /** Example of custom column using a closure **/
+            ->addColumn('office_name_lower', function (Office $model) {
+                return strtolower(e($model->office_name));
+            })
+
+            ->addColumn('getDepartmentName.department_name')
+            ->addColumn('getDistrictName.district_name')
+            // ->addColumn('in_area')
+            // ->addColumn('rural_block_code')
+            // ->addColumn('gp_code')
+            // ->addColumn('urban_code')
+            // ->addColumn('ward_code')
+            ->addColumn('office_address')
+            ->addColumn('level_no', function (Office $model) {
+                switch ($model->level_no) {
+                    case 1:
+                        return '<span class="badge rounded-pill bg-primary text-dark">Level 1 Office </span>';
+                        break;
+                    case 2:
+                        return '<span class="badge rounded-pill bg-secondary">Level 2 Office </span>';
+                        break;
+                    case 3:
+                        return '<span class="badge rounded-pill bg-success">Level 3 Office </span>';
+                        break;
+                    case 4:
+                        return '<span class="badge rounded-pill bg-info">Level 4 Office </span>';
+                        break;
+                    case 5:
+                        return '<span class="badge rounded-pill bg-warning">Level 5 Office </span>';
+                        break;
+                    default:
+                        return '<span class="badge rounded-pill bg-dark">Level 6 Office</span>';
+                }
+            })
+            ->addColumn('office_code');
+        // ->addColumn('created_at_formatted', fn (Office $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'))
+        // ->addColumn('updated_at_formatted', fn (Office $model) => Carbon::parse($model->updated_at)->format('d/m/Y H:i:s'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+    */
+
+    /**
+     * PowerGrid Columns.
+     *
+     * @return array<int, Column>
+     */
     public function columns(): array
     {
         return [
-            // Column::make("Id", "id")
-            //     ->sortable(),
-            Column::make("Office name", "office_name")
-                ->sortable(),
-            Column::make("Office Code", "office_code")
-                ->sortable(),
-            Column::make("Office Level", "level_no")
-                ->format(function ($value, $column, $row) {
-                    switch ($value) {
-                        case 1:
-                            return '<span class="badge rounded-pill bg-primary text-dark">Level 1 Office </span>';
-                            break;
-                        case 2:
-                            return '<span class="badge rounded-pill bg-secondary">Level 2 Office </span>';
-                            break;
-                        case 3:
-                            return '<span class="badge rounded-pill bg-success">Level 3 Office </span>';
-                            break;
-                        case 4:
-                            return '<span class="badge rounded-pill bg-info">Level 4 Office </span>';
-                            break;
-                        case 5:
-                            return '<span class="badge rounded-pill bg-warning">Level 5 Office </span>';
-                            break;
-                        default:
-                            return '<span class="badge rounded-pill bg-dark">Level 6 Office</span>';
-                    }
-                })
-                ->html()
-                ->sortable(),
-            Column::make("Department Name", "getDepartmentName.department_name")
-                ->sortable(),
-            Column::make("Dist Name", "getDistrictName.district_name")
-                ->sortable(),
-            // Column::make("In area", "In_area")
-            //     ->sortable(),
-            // Column::make("Rural block Name", "rural_block_code")
-            //     ->sortable(),
-            // Column::make("Gp Name", "gp_code")
-            //     ->sortable(),
-            // Column::make("Urban Name", "getUrban.urban_body_name")
-            //     ->sortable(),
-            // Column::make("Ward Name", "ward_code")
-            //     ->sortable(),
-            Column::make("Office address", "office_address")
-                ->sortable(),
-            // Column::make("Created at", "created_at")
-            //     ->sortable(),
-            // Column::make("Updated at", "updated_at")
-            //     ->sortable(),
+            // Column::make('ID', 'id')
+            //     ->makeInputRange(),
+
+            Column::make('OFFICE NAME', 'office_name')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            Column::make('DEPARTMENT name', 'getDepartmentName.department_name')
+                ->makeInputRange(),
+
+            Column::make('DIST name', 'getDistrictName.district_name')
+                ->makeInputRange(),
+
+            // Column::make('IN AREA', 'in_area')
+            //     ->makeInputRange(),
+
+            // Column::make('RURAL BLOCK CODE', 'rural_block_code')
+            //     ->makeInputRange(),
+
+            // Column::make('GP CODE', 'gp_code')
+            //     ->makeInputRange(),
+
+            // Column::make('URBAN CODE', 'urban_code')
+            //     ->makeInputRange(),
+
+            // Column::make('WARD CODE', 'ward_code')
+            //     ->makeInputRange(),
+
+            Column::make('OFFICE ADDRESS', 'office_address')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('LEVEL NO', 'level_no')
+                ->makeInputRange(),
+
+            Column::make('OFFICE CODE', 'office_code')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            // Column::make('CREATED AT', 'created_at_formatted', 'created_at')
+            //     ->searchable()
+            //     ->sortable()
+            //     ->makeInputDatePicker(),
+
+            // Column::make('UPDATED AT', 'updated_at_formatted', 'updated_at')
+            //     ->searchable()
+            //     ->sortable()
+            //     ->makeInputDatePicker(),
+
         ];
     }
-    public function builder(): Builder
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable the method below only if the Routes below are defined in your app.
+    |
+    */
+
+    /**
+     * PowerGrid Office Action Buttons.
+     *
+     * @return array<int, Button>
+     */
+
+    /*
+    public function actions(): array
     {
-        return Office::query()
-            ->where('offices.department_id', Auth::user()->department_id);
+       return [
+           Button::make('edit', 'Edit')
+               ->class('bg-indigo-500 cursor-pointer text-white px-3 py-2.5 m-1 rounded text-sm')
+               ->route('office.edit', ['office' => 'id']),
+
+           Button::make('destroy', 'Delete')
+               ->class('bg-red-500 cursor-pointer text-white px-3 py-2 m-1 rounded text-sm')
+               ->route('office.destroy', ['office' => 'id'])
+               ->method('delete')
+        ];
     }
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Rules
+    |--------------------------------------------------------------------------
+    | Enable the method below to configure Rules for your Table and Action Buttons.
+    |
+    */
+
+    /**
+     * PowerGrid Office Action Rules.
+     *
+     * @return array<int, RuleActions>
+     */
+
+    /*
+    public function actionRules(): array
+    {
+       return [
+
+           //Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($office) => $office->id === 1)
+                ->hide(),
+        ];
+    }
+    */
 }
