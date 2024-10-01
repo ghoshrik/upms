a2be73ffd0d0b70e95c47efbe6560db2cce1e97a
Avinash kumar <62693318+avi2727@users.noreply.github.com>
2024-10-01 13:06:56 +0530

    Added design migration

---
+++ database/migrations/2024_09_30_153356_create_designs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDesignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('project_id')->references('project_id')->on('project_creations');
+            $table->foreignId('document_type_id')->references('id')->on('document_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
