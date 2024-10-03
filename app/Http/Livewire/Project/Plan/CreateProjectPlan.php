93eca1cebd6dc44022b24b461af1b57535d3c7cb
Avinash kumar <62693318+avi2727@users.noreply.github.com>
2024-10-03 18:18:50 +0530

    Added validation edit and delete feature, and pagination

--- app/Http/Livewire/Project/Plan/CreateProjectPlan.php
+++ app/Http/Livewire/Project/Plan/CreateProjectPlan.php
@@ -12,35 +12,67 @@ class CreateProjectPlan extends Component
 {
     use Actions;
     protected $listeners = ['openAddPlan'];
-    public $modal = false, $project, $title;
-
-    public function openAddPlan()
+    public $modal = false, $project, $title, $editplanId;
+    protected $rules = [
+        'title' => 'required|string|max:255',
+    ];
+    public function openAddPlan($id = '')
     {
+        $this->resetValidation();
         $this->modal = !$this->modal;
+        if (!empty($id)) {
+            $plan = Plan::find($id);
+            if ($plan) {
+                $this->editplanId = $id;
+                $this->title = $plan->title;
+            } else {
+                $this->resetInput();
+                $this->notification()->error('Error', 'Plan not found!');
+            }
+        } else {
+            $this->resetInput();
+        }
     }
+    private function resetInput()
+    {
+        $this->editplanId = null;
+        $this->title = '';
+    }
+
     public function store()
     {
+        $this->validate();
         DB::beginTransaction();
-
         try {
-            $insert = [
+            $data = [
                 'title' => $this->title,
                 'project_creation_id' => $this->project->id,
                 'department_id' => $this->project->department_id,
                 'created_by' => Auth::user()->id,
             ];
-
-            Plan::create($insert);
+            if ($this->editplanId) {
+                $plan = Plan::find($this->editplanId);
+                if ($plan) {
+                    $plan->update($data);
+                    $this->notification()->success('Plan Updated', 'Plan details were updated successfully!');
+                } else {
+                    throw new \Exception('Plan not found.');
+                }
+            } else {
+                Plan::create($data);
+                $this->notification()->success('Plan Created', 'New plan was created successfully!');
+            }
             $this->resetExcept('project');
-            $this->emit('refreshPlanTableData');
-            $this->notification()->success('Project Updated', 'Project details were updated successfully!');
+
 
             DB::commit();
         } catch (\Exception $e) {
             DB::rollBack();
-            $this->notification()->error('Error', 'There was an error updating the project: ' . $e->getMessage());
+            $this->notification()->error('Error', 'There was an error saving the plan: ' . $e->getMessage());
         }
+        $this->emit('refreshPlanTableData');
     }
+
     public function render()
     {
         return view('livewire.project.plan.create-project-plan');
--- resources/views/livewire/project/plan/create-project-plan.blade.php
+++ resources/views/livewire/project/plan/create-project-plan.blade.php
@@ -1,27 +1,33 @@
 <div>
     <x-modal max-width="xl" blur wire:model.defer="modal">
         <x-card>
-            <div class="row">
-                <div class="col-md-12 col-lg-12 col-sm-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $project->name }}</h5>
-                            <div class="row">
-                                <div class="form-group">
-                                    <x-input wire:key="title" label="Plan Title" placeholder="Enter Plan Title"
-                                        wire:model.defer="title" />
-                                </div>
-                            </div>
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h5 class="modal-title">{{ $project->name }}</h5>
+                <button type="button" class="close" aria-label="Close" x-on:click="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <!-- Modal Body -->
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <x-input wire:key="title" label="Plan Title" placeholder="Enter Plan Title"
+                                wire:model.defer="title" />
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Modal Footer -->
             <x-slot name="footer">
                 <div class="flex justify-between">
-                    <div class="flex float-left">
+                    <div>
                         <x-button class="rounded btn btn-soft-danger" flat label="Cancel" x-on:click="close" />
                     </div>
-                    <div class="flex float-right">
+                    <div>
                         <button type="button" wire:click="store" class="ml-auto rounded btn btn-soft-success">Save</button>
                     </div>
                 </div>
