5d764eb3f6d6dcb3a89b1b065ae69f186b6bf627
Avinash kumar <62693318+avi2727@users.noreply.github.com>
2024-10-03 18:23:03 +0530

    Document type seeder

---
+++ database/seeders/DocumentTypeSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+class DocumentTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $doctypes = [
+            'ADCP survey report',
+            'Soil Testing report',
+            'Photograph with geocoding',
+            'Basic survey report',
+            'Design philosophy',
+            'Design document',
+            'Meeting with citizen report'
+        ];
+
+        foreach ($doctypes as $doctype) {
+            DB::table('document_types')->insert([
+                'name' => $doctype,
+                'created_by' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
